92d3722276b00727bcc83443d5275414d854e3a0
David Jenkins <davidjenkins479@gmail.com>
2025-02-18 11:12:40 +0100

    reorganise project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
-import { HomePage } from "./components/HomePage";
-import { AboutUs } from "./Pages/AboutPage/AboutUs";
-import { AddRestaurantPage } from "./Pages/AddRestaurantPage/AddRestaurantPage";
-import { TableBooking } from "./Pages/BookTablePage/TableBooking";
-import Navbar from './components/Navbar';
+import AddRestaurant from './Pages/AddResturant/AddRestaurant';
+import BookTable from './Pages/BookTable/BookTable';
+import AboutUs from './Pages/AboutUs';
+import NotFoundPage from './components/NotFoundPage';
+import RestaurantDetails from './Pages/RestaurantDetails/RestaurantDetails';
 
 
 function App() {
@@ -14,9 +14,9 @@ function App() {
        <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="about" element={<AboutUs />} />
-        <Route path="book-a-table" element={<TableBooking/>} />
-        <Route path="details" element={<RestaurantDetailsPage/>} />
-        <Route path="add-restaurant" element={<AddRestaurantPage/>} />
+        <Route path="book-a-table" element={<BookTable/>} />
+        <Route path="details" element={<RestaurantDetails/>} />
+        <Route path="add-restaurant" element={<AddRestaurant/>} />
         <Route path="*" element={<NotFoundPage/>} /> {/* 404 Page */}
        </Routes>
     </Router>
