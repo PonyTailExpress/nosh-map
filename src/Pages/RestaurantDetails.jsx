f02a7c77171fabb4f9858fa979b2b1a564da33f1
David Jenkins <davidjenkins479@gmail.com>
2025-02-21 08:36:01 +0100

    fix minor typo issue

--- src/Pages/RestaurantDetails.jsx
+++ src/Pages/RestaurantDetails.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import apiClient from "../Services/api";
+import apiClient from "../services/api";
 
 function RestaurantDetails() {
   const { id } = useParams(); // Get the restaurant ID from the URL
